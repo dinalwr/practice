b7a114c6caff0a1c411d995ec3093953abc2ea15
dinalwr <dianok1105@gmail.com>
2023-05-05 21:15:59 +0700

    Add first part

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/* Функция скрытия/показа navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+/* функция открытия меню-бургер */
+$(document).ready(function () {
+  $('.header-burger').click(function (event) {
+    $('.header-burger, .header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
